e505d5cc4f7e71d02e15906caaeee7a410762dfd
Nick Geoca <nickgeoca@gmail.com>
2017-07-23 22:38:51 -0500

    SimpleStorage works on testrpc and geth

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,16 @@
-web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
-
-// var TestRPC = require("ethereumjs-testrpc");
-// web3.setProvider(TestRPC.provider());
-
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var RezToken = artifacts.require('./RezToken.sol');
-var RezSafe = artifacts.require('./RezSafe.sol');
+var RezToken = artifacts.require('RezToken.sol');
+var RezSafe = artifacts.require('RezSafe.sol');
 
 module.exports = (deployer, network, accounts) => {
+  deployer.deploy(SimpleStorage);
+
   let timeTilStart = 0
   let timeDuration = 60
 
-  let currentBlock = web3.eth.blockNumber;
-  let startBlock = Math.round(currentBlock + timeTilStart / 1.0) 
-  let endBlock   = Math.round(startBlock + timeDuration / 1.0) 
-  deployer.deploy(SimpleStorage);
+  let currentBlock = web3.eth.blockNumber
+  let startBlock = Math.round(currentBlock + timeTilStart / 14.0) 
+  let endBlock   = Math.round(startBlock + timeDuration / 14.0) 
 
   deployer.deploy(RezToken, accounts[0], accounts[0], startBlock, endBlock)
     .then(()    => RezToken.deployed())

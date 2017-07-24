b5046d3f71db2bbf1b23fff7d843e6abf5efa26e
Nick Geoca <nickgeoca@gmail.com>
2017-07-23 19:16:02 -0500

    Working async/await

--- src/App.js
+++ src/App.js
@@ -51,19 +51,11 @@ class App extends Component {
     var simpleStorageInstance
 
     // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+    this.state.web3.eth.getAccounts(async (e, accounts) => {
+      let instance = await simpleStorage.deployed();
+      await instance.set(33, {from: accounts[0]});
+      let result   = await instance.get.call(accounts[0]);
+      return         await this.setState({ storageValue: result.c[0] });
     })
   }
 
